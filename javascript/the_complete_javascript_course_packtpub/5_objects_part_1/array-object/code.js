d65035786d311422aaf88d880ce4c04a457292e4
liegele <liegele@gmail.com>
2022-07-05 18:26:06 -0300

    Atualizando o code.js com array methods

---
+++ javascript/the_complete_javascript_course_packtpub/5_objects_part_1/array-object/code.js
@@ -0,0 +1,32 @@
+let shoppingList = ['orange', 'apple', 'banana'];
+console.log(shoppingList);
+console.log(typeof shoppingList);
+
+//Array constructor
+let shoppingList2 = new Array('bread', 'eggs', 'milk');
+console.log(shoppingList2);
+console.log(typeof shoppingList2);
+
+console.log(shoppingList[1]);
+
+//Add an element at the end
+shoppingList.push('pear');
+console.log(shoppingList);
+console.log(shoppingList.sort());
+console.log(shoppingList.reverse());
+
+//Sorting an array
+let numbers = [12, 4, 67, 14, 2, 7, 99, 53];
+console.log(numbers);
+let numbersSorted = numbers.sort();
+console.log(numbersSorted);
+
+//Map
+let addDelicious = function(n) {
+    return 'Delicious ' + n;
+}
+
+let x = shoppingList.map(addDelicious);
+console.log(x);
+
+//Dell na bateria às 18:24
