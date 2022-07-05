681d4f30bcbb231e38c413cf7e94330155168c97
liegele <liegele@gmail.com>
2022-07-05 17:41:06 -0300

    Atualizando o code.js

--- javascript/the_complete_javascript_course_packtpub/5_objects_part_1/json/code.js
+++ javascript/the_complete_javascript_course_packtpub/5_objects_part_1/json/code.js
@@ -1,9 +1,48 @@
 //Let's create a object
 let person = {
-    name: 'Liégele Cabral',
-    state: 'Bahia',
+    name: "Liégele Cabral",
+    state: "Brasília",
     birth: 1976
-}
+};
 
-//Convert to a JSON bject
+//JSON object
+let personJSON = {
+    "name": "Liégele Cabral",
+    "state": "Brasília",
+    "birth": 1976
+};
 
+//JSON array
+let personJSON2 = [
+    {
+        "name": "Liégele Cabral",
+        "state": "BahBrasíliaia",
+        "birth": 1976
+    },
+    {
+        "name": "Anizia Oliveira",
+        "state": "Sergipe",
+        "birth": 1976
+    },
+    {
+        "name": "Gabriel Oliveira",
+        "state": "Bahia",
+        "birth": 1976
+    }
+
+];
+
+console.log(personJSON);
+console.log(typeof personJSON);
+console.log(personJSON2);
+console.log(typeof personJSON2);
+
+//Convert to a JSON string
+let personString = JSON.stringify(personJSON);
+console.log(personString);
+console.log(typeof personString);
+
+//Parse a JON string
+var personParsedAsJSON = JSON.parse(personString);
+console.log(personParsedAsJSON);
+console.log(typeof personParsedAsJSON);
