aacde07aa4989cfa7815a0bd83abf2d737b36b47
liegele <liegele@gmail.com>
2022-07-06 22:12:13 -0300

    Atualizando arquivos de código

---
+++ javascript/the_complete_javascript_course_packtpub/6_the_dom/understand-the-dom/code.js
@@ -0,0 +1,23 @@
+// console.log(window);
+
+//Access an element by ID
+console.log(document.getElementById('header'));
+let header = document.getElementById('header');
+console.log(typeof header);
+console.log(header.innerHTML);
+
+//Access children of a node
+let bodyChildren = document.body.children;
+console.log(bodyChildren);
+
+//Add a new child to the body
+let p = document.createElement('p');
+console.log(p);
+let textNode = document.createTextNode('A new paragraph.');
+p.appendChild(textNode);
+console.log(p);
+document.body.appendChild(p);
+console.log(window.document);
+
+
+
