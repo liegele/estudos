75d014091ade39587b1a7e225bd389c580dda46e
liegele <liegele@gmail.com>
2022-07-13 19:11:45 -0300

    Atualizando o codigo

--- javascript/the_complete_javascript_course_packtpub/6_the_dom/event-handling/code.js
+++ javascript/the_complete_javascript_course_packtpub/6_the_dom/event-handling/code.js
@@ -2,6 +2,34 @@ let f1 = function(){
     console.log('You clicked on button1.')
 }
 
+let f2 = function(){
+    console.log('Button1 clicked');
+}
+
 //Add a function to button1
 let button1 = document.getElementById('button1');
-button1.onclick = f1;
+/* button1.onclick = f1;
+button1.onclick = f2; */
+
+//Add an eventListener to the button1
+button1.addEventListener('click', f1);
+button1.addEventListener('click', f2);
+
+//Removing a action from eventListener
+button1.removeEventListener('click', f2);
+
+//Add a action to button2
+let button2 = document.getElementById('button2');
+let p1 = document.getElementById('p1');
+let flagColor = 0;
+// console.log(typeof(flagColor));
+button2.onclick = function(){
+    if(flagColor == 0){
+        p1.style.color = 'rgb(43, 174, 226)';
+        flagColor = 1;
+    } else {
+        p1.style.color = 'rgb(186, 192, 192)';
+        flagColor = 0;
+    }
+}
+
