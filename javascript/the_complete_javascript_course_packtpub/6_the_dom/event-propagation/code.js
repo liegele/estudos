a08778949cad0de7431a4d80d6c014547034bd00
liegele <liegele@gmail.com>
2022-07-15 17:09:46 -0300

    Atualizando arquivos

---
+++ javascript/the_complete_javascript_course_packtpub/6_the_dom/event-propagation/code.js
@@ -0,0 +1,21 @@
+let div = document.querySelector('div');
+let p = document.querySelector('p');
+let span = document.querySelector('span');
+
+//functions
+function logDiv(){
+    console.log('Div!');
+}
+
+function logP(){
+    console.log('Paragraph!');
+}
+
+function logSpan(){
+    console.log('Span!');
+}
+
+//Event Handlers
+div.addEventListener('click', logDiv, true);
+p.addEventListener('click', logP, true);
+span.addEventListener('click', logSpan);
