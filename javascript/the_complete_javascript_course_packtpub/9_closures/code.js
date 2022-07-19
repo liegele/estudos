2945b20290df2348f24548278b0a53ee179f8b85
liegele <liegele@gmail.com>
2022-07-19 17:22:35 -0300

    Atualizando código

---
+++ javascript/the_complete_javascript_course_packtpub/9_closures/code.js
@@ -0,0 +1,44 @@
+/* //Try this first
+function add1(){
+    let x = 1;
+    let f = function(y){
+        return x + y;
+    }
+    return f(3);
+}
+
+console.log(add1()); */
+
+//Try this
+let add = function(){
+    let x = 1;
+    let f = function(y){
+        return x + y;
+    };
+    return f;
+}
+
+let g = add();
+console.log(g(2));
+
+//Update values of outer vaiables
+let n;
+function f(x){
+    n = function(){
+        return x;
+    };
+    x ++;
+}
+
+f(123);
+console.log(n());
+
+//Global EC (Execution Context)
+    //=>n = undefined
+    //=>f
+
+//f EC
+    //=>n = function(){return x}
+    //=>x = 124
+//n EC
+    //=>x = 124
