56abedc17cab20b755b4bcd51cc8e792f26f9cf0
liegele <liegele@gmail.com>
2022-07-26 16:58:31 -0300

    Atualizando arquivos

---
+++ javascript/the_complete_javascript_course_packtpub/10_objects_part_2/why_would_you_hide_information/code.js
@@ -0,0 +1,23 @@
+/* class phone {
+    constructor(make, model, warranty) {
+        this.make = make;
+        this.model = model;
+        this.warranty = warranty;
+    }
+} */
+
+let phone = function(make, model, warranty){
+    this.make = make;
+    this.model = model;
+    this.warranty = warranty || 12; //Fallback if no warranty is assigned.
+}
+
+phone.prototype = {
+    extendWarranty(x){
+        this.warranty += x;
+    }
+}
+
+let myPhone = new phone('iPhone', '13');
+myPhone.extendWarranty(24);
+console.log(myPhone.warranty);
