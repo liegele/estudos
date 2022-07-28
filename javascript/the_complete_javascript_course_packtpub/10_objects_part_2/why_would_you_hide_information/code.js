df64f717e2a4d98aad35e3b4415a1f45fd60c644
liegele <liegele@gmail.com>
2022-07-27 23:04:53 -0300

    Atualizando arquivos

--- javascript/the_complete_javascript_course_packtpub/10_objects_part_2/why_would_you_hide_information/code.js
+++ javascript/the_complete_javascript_course_packtpub/10_objects_part_2/why_would_you_hide_information/code.js
@@ -1,12 +1,21 @@
-/* class phone {
-    constructor(make, model, warranty) {
-        this.make = make;
-        this.model = model;
-        this.warranty = warranty;
-    }
-} */
+class Phone {
 
-let phone = function(make, model, warranty){
+    #make;
+    #model;
+    #warranty;
+
+    constructor(make, model, warranty) {
+        this.#make = make;
+        this.#model = model;
+        this.#warranty = warranty || 12;
+    }
+
+    extendWarranty(x){
+        this.#warranty += 12;
+    }
+}
+
+/* let phone = function(make, model, warranty){
     this.make = make;
     this.model = model;
     this.warranty = warranty || 12; //Fallback if no warranty is assigned.
@@ -16,11 +25,13 @@ phone.prototype = {
     extendWarranty(x){
         this.warranty += x;
     }
-}
+} */
 
-let myPhone = new phone('iPhone', '13');
+let myPhone = new Phone('iPhone', '13');
 myPhone.extendWarranty(24);
 console.log(myPhone.make);
 console.log(myPhone.warranty);
+myPhone.make = '';
+console.log(myPhone.make);
 
 //Create Setters and Getters
