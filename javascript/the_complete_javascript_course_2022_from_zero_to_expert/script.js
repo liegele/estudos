79e1bc6c1c8a068c8729ae2539ceb2b7a42a7aa0
liegele <liegele@gmail.com>
2022-08-11 15:46:31 -0300

    Atualizando arquivos

--- javascript/the_complete_javascript_course_2022_from_zero_to_expert/script.js
+++ javascript/the_complete_javascript_course_2022_from_zero_to_expert/script.js
@@ -1,5 +1,5 @@
 const markMass = 78;
-const markHeight = 1.69;
+const markHeight = 1.99;
 const johnMass = 92;
 const johnHeight = 1.95;
 
@@ -9,5 +9,11 @@ console.log(bmiMark);
 const bmiJohn = (johnMass)/(johnHeight ** 2);
 console.log(bmiJohn);
 
-const markHigherBMI = bmiMark > bmiJohn;
-console.log(`Is Mark BMI higher than John? ${markHigherBMI}`);
+// const markHigherBMI = bmiMark > bmiJohn;
+// console.log(`Is Mark BMI higher than John? Answer: ${markHigherBMI}`);
+
+if (bmiMark > bmiJohn) {
+  console.log(`Mark's BMI(${bmiMark.toPrecision(3)}) is higher than John'sBMI(${bmiJohn.toPrecision(3)}).`);
+} else {
+  console.log(`John's BMI(${bmiJohn.toPrecision(3)}) is higher than Mark'sBMI(${bmiMark.toPrecision(3)}).`);
+}
