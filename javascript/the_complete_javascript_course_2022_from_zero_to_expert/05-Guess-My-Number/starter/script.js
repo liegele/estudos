ed86c3d971e1be897b318891645281219027cf76
liegele <liegele@gmail.com>
2022-08-24 21:57:42 -0300

    Atualizando arquivos

--- javascript/the_complete_javascript_course_2022_from_zero_to_expert/05-Guess-My-Number/starter/script.js
+++ javascript/the_complete_javascript_course_2022_from_zero_to_expert/05-Guess-My-Number/starter/script.js
@@ -11,6 +11,7 @@ console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 17; */
 
 let score = 20;
+let highScore = 0;
 
 function updateScore(){
     score --;
@@ -18,20 +19,32 @@ function updateScore(){
 }
 
 const secretNumber = Math.trunc((Math.random() * 20) + 1);
-document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.number').textContent = secretNumber;
 
-document.querySelector('.check').addEventListener('click', function(){
+document.querySelector('html').addEventListener('onload', function() {
+    
+});
+
+document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
 
     if (score > 1) {
+
         if (guess < 1) {
+            //When there is no input
             document.querySelector('.message').textContent = '❌ Less than 1.';
         } else if (guess === secretNumber) {
+            //When player wins
             document.querySelector('.message').textContent = '🎉 Correct Number!';
+            document.querySelector('body').style.backgroundColor = '#60b347';
+            document.querySelector('.number').style.width = '30rem'
+            document.querySelector('.number').textContent = secretNumber;
         } else if (guess > secretNumber) {
+            //When guess is too high
             document.querySelector('.message').textContent = '🔼 Too high!';
             updateScore();
         } else if (guess < secretNumber) {
+            //When guess is too low
             document.querySelector('.message').textContent = '🔽 Too low!';
             updateScore();
         }
@@ -39,6 +52,15 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('.message').textContent = '💥 You lose!!!';
         updateScore();
     }
-
-
 });
+
+//When player press Again button
+document.querySelector('.again').addEventListener('click', function() {
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('body').style.backgroundColor = '#222222';
+    document.querySelector('.number').style.width = '15rem'
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '',
+    document.querySelector('.score').textContent = '20';
+    score = 20;
+})
