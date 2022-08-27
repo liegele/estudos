b93cdf967f79934cfd2b1c46579b7d03b46e342d
liegele <liegele@gmail.com>
2022-08-26 22:38:53 -0300

    Atualizando arquivos

--- javascript/the_complete_javascript_course_2022_from_zero_to_expert/06-Modal/starter/script.js
+++ javascript/the_complete_javascript_course_2022_from_zero_to_expert/06-Modal/starter/script.js
@@ -4,3 +4,26 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
+
+console.log(btnShowModal);
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  console.log(btnShowModal[i].addEventListener('click', openModal));
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+/* window.addEventListener('keydown', function (e) {
+  console.log(e);
+}); */
