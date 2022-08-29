6752f6d354201244fcf24cb787382aa6641fd18a
liegele <liegele@gmail.com>
2022-08-29 14:57:56 -0300

    Atualizando arquivos

--- javascript/the_complete_javascript_course_2022_from_zero_to_expert/07-Pig-Game/starter/script.js
+++ javascript/the_complete_javascript_course_2022_from_zero_to_expert/07-Pig-Game/starter/script.js
@@ -6,12 +6,23 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
-const btnRoll = document.querySelector('.btn--roll');
 const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('btn-hold');
 
+//Startin conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
-
-//Hiding the dice at the beggining of game
 diceEl.classList.add('hidden');
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  //2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3. Check for rolled 1: if true, switch to next payer
+});
