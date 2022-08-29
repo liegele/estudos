e431327aeb55f68e044f07c037d35f2d4b93d41c
liegele <liegele@gmail.com>
2022-08-29 14:43:29 -0300

    Atualizando arquivos

--- javascript/the_complete_javascript_course_2022_from_zero_to_expert/07-Pig-Game/starter/script.js
+++ javascript/the_complete_javascript_course_2022_from_zero_to_expert/07-Pig-Game/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// const score1 = document.getElementById('score--1'); //A little faster than querySelector
+
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('btn-hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+//Hiding the dice at the beggining of game
+diceEl.classList.add('hidden');
