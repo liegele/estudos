b1b891cb83fd1c825946f82d210fa460dc72b9df
liegele <liegele@gmail.com>
2022-09-05 15:04:10 -0300

    Atualizando arquivos

--- javascript/the_complete_javascript_course_2022_from_zero_to_expert/08-Behind-the-Scenes/starter/script.js
+++ javascript/the_complete_javascript_course_2022_from_zero_to_expert/08-Behind-the-Scenes/starter/script.js
@@ -59,3 +59,29 @@ console.log('Before marriage: ', jessica);
 console.log('After marriage: ', marriedJessica);
 
 // marriedJessica = {};
+
+//Copying objects (create a shallow copy)
+
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+// console.log(jessicaCopy.family);
+
+console.log('-------');
+console.log('Before marriage: ', jessica2);
+console.log('After marriage: ', jessicaCopy);
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('-------');
+console.log('Before marriage: ', jessica2);
+console.log('After marriage: ', jessicaCopy);
+
+jessicaCopy2 = new jessica2();
