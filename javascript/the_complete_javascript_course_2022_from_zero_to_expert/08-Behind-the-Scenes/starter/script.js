e0d10e8cac481f96670cbad1f159a9359ea33aef
liegele <liegele@gmail.com>
2022-09-05 14:32:29 -0300

    Atualizando arquivos

--- javascript/the_complete_javascript_course_2022_from_zero_to_expert/08-Behind-the-Scenes/starter/script.js
+++ javascript/the_complete_javascript_course_2022_from_zero_to_expert/08-Behind-the-Scenes/starter/script.js
@@ -1,39 +1,61 @@
 'use strict';
 
-//Understanding how THIS keyword works...
+// //Understanding how THIS keyword works...
 
-console.log(this);
+// console.log(this);
 
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  console.log(this); //undefined just because the 'strict mode'.
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this); //undefined just because the 'strict mode'.
+// };
+
+// calcAge(1976);
+
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this); //Arrow function doesn't have your own this keyword. This way it takes from parent scope (Window Object) in this case.
+// };
+
+// calcAgeArrow(1976);
+
+// const jonas = {
+//   year: 1991,
+//   calcAge() {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+// };
+
+// jonas.calcAge();
+
+// const matilda = {
+//   year: 2017,
+// };
+
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// f();
+
+//Primitives vs Objects in Practice
+
+let lastName = 'Williams';
+let odLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, odLastName);
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
 };
 
-calcAge(1976);
+const marriedJessica = jessica;
 
-const calcAgeArrow = birthYear => {
-  console.log(2037 - birthYear);
-  console.log(this); //Arrow function doesn't have your own this keyword. This way it takes from parent scope (Window Object) in this case.
-};
+marriedJessica.lastName = 'Davis';
 
-calcAgeArrow(1976);
+console.log('Before marriage: ', jessica);
+console.log('After marriage: ', marriedJessica);
 
-const jonas = {
-  year: 1991,
-  calcAge() {
-    console.log(this);
-    console.log(2037 - this.year);
-  },
-};
-
-jonas.calcAge();
-
-const matilda = {
-  year: 2017,
-};
-
-matilda.calcAge = jonas.calcAge;
-matilda.calcAge();
-
-const f = jonas.calcAge;
-f();
+// marriedJessica = {};
