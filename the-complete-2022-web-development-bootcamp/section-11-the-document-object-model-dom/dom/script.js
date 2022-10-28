94e6051174c66e504fe81baf5ad30afa3d4b498f
liegele <liegele@gmail.com>
2022-10-27 23:15:57 -0300

    Atualizando arquivos

--- the-complete-2022-web-development-bootcamp/section-11-the-document-object-model-dom/dom/script.js
+++ the-complete-2022-web-development-bootcamp/section-11-the-document-object-model-dom/dom/script.js
@@ -1,2 +1,14 @@
 const header = document.getElementById("mainHeader");
-header.textContent = "Good Bye!";
+header.classList.add("huge");
+
+/* header.style.color = "red";
+header.style.fontSize = "10rem";
+// header.style.letterSpacing = "-0.5rem";
+
+console.log((document.querySelector("li a").style.color = "red"));
+
+const btn = document.querySelector("button");
+btn.style.backgroundColor = "yellow"; */
+
+console.log(document.querySelector("a").getAttribute("href"));
+document.querySelector("a").setAttribute("href", "http://www.bing.com");
