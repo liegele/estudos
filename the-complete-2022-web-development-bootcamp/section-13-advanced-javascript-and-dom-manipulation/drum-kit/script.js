b6c3b284341952c6792827c3783ca9be5699f87f
liegele <liegele@gmail.com>
2022-10-29 14:41:38 -0300

    Atualizando arquivos

--- the-complete-2022-web-development-bootcamp/section-13-advanced-javascript-and-dom-manipulation/drum-kit/script.js
+++ the-complete-2022-web-development-bootcamp/section-13-advanced-javascript-and-dom-manipulation/drum-kit/script.js
@@ -9,27 +9,34 @@ function playSound(sound) {
 
 //Setting addEventListener to drum buttons.
 for (let i = 0; i < numberOfDrumButtons; i++) {
-  const drumBtn = document
+  let drumBtn = document
     .querySelectorAll(".drum")
-    [i].addEventListener("click", (e) => {
-      /* switch (e.key) {
-      alert(e.key);
-      console.log(e.key);
+    [i].addEventListener("click", () => {
+      drumBtn = document.querySelectorAll(".drum")[i].textContent;
+      console.log(drumBtn);
+      switch (drumBtn) {
         case "q":
+          playSound("tom-1.mp3");
           break;
         case "w":
+          playSound("tom-2.mp3");
           break;
         case "e":
+          playSound("tom-3.mp3");
           break;
         case "r":
+          playSound("kick-bass.mp3");
           break;
         case "i":
+          playSound("tom-4.mp3");
           break;
         case "o":
+          playSound("snare.mp3");
           break;
         case "p":
+          playSound("crash.mp3");
           break;
-      } */
+      }
     });
 }
 
