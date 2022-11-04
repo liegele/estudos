cf1790ee8b99a02419d9d34d1b403cf330d2301f
liegele <liegele@gmail.com>
2022-11-04 14:32:18 -0300

    Atualizando arquivos

--- the-complete-2022-web-development-bootcamp/section-15-boss-level-challenge-the-simon-game/script.js
+++ the-complete-2022-web-development-bootcamp/section-15-boss-level-challenge-the-simon-game/script.js
@@ -1,23 +1,23 @@
 let gamePattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
 
-//Function to generate a random number
+//Function to generate a random number.
 function nextSequence() {
   return Math.floor(Math.random() * 4);
 }
 
-//Defining the chosen colour
+//Defining the chosen colour.
 let randomChosenColour = buttonColours[nextSequence()];
 console.log(randomChosenColour);
 
-//Put chosen colour in gamePattern array
+//Put chosen colour in gamePattern array.
 gamePattern.push(randomChosenColour);
 console.log(gamePattern);
 
 //Adding fadeOut and fadeIn effect on chosen colour.
 $(`#${randomChosenColour}`).delay().fadeOut().fadeIn();
 
-//Playing a sound for chosen colour
+//Playing a sound for chosen colour.
 function playSound() {
   let audio = new Audio(`sounds/${randomChosenColour}.mp3`);
   audio.play();
