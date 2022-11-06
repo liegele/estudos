c1aef52af27faf19d2202751e717e02ce3816d5b
liegele <liegele@gmail.com>
2022-11-06 03:03:43 -0300

    Atualizando arquivos

--- the-complete-2022-web-development-bootcamp/section-15-boss-level-challenge-the-simon-game/script.js
+++ the-complete-2022-web-development-bootcamp/section-15-boss-level-challenge-the-simon-game/script.js
@@ -1,59 +1,80 @@
 let gamePattern = [];
 let userClickedPattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
-
 let randomNumber, randomChosenColour;
+let level = 1;
+let gameStarted = false;
 
+//Starting game when 'A' key is pressed.
 $("body").on("keydown", (e) => {
-  if (e.key === "a") {
+  if (e.key === "a" && gameStarted === false) {
+    gameStarted = true;
+    updateUI(`Level ${level}`);
     gamePattern = [];
     nextSequence();
   }
 });
 
-//Function to keep the sequence of sounds.
-function nextSequence() {
-  //Generating a random number between [0 and 3].
-  randomNumber = Math.floor(Math.random() * 4);
-
-  //Defining the chosen colour.
-  randomChosenColour = buttonColours[randomNumber];
-  // console.log(randomChosenColour);
-
-  //Put chosen colour in gamePattern array.
-  gamePattern.push(randomChosenColour);
-  // console.log(gamePattern);
-
-  //Adding fadeOut and fadeIn effect on chosen colour.
-  $(`#${randomChosenColour}`).delay().fadeOut().fadeIn();
-
-  playSound(randomChosenColour);
+//Update the user interface.
+function updateUI(message) {
+  $("#level-title").text(message);
 }
 
-// nextSequence();a
+//Function to keep the sequence of colours/sounds.
+function nextSequence() {
+  randomNumber = Math.floor(Math.random() * 4);
+  randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  gamePattern.forEach((element) => {
+    setTimeout(() => {
+      $(`#${element}`).delay().fadeOut().fadeIn();
+      playSound(element);
+    }, 1000);
+  });
+
+  gamePattern.forEach(() => {
+    new Promise((resolve, reject) => {
+      resolve(setTimeout(() => {}, 1000));
+    }).then(() => {
+      (element) => {
+        setTimeout(() => {
+          $(`#${element}`).delay().fadeOut().fadeIn();
+          playSound(element);
+        }, 1000);
+      };
+    });
+  });
+
+  updateUI(`Level ${level}`);
+  console.log(gamePattern);
+  level++;
+}
 
 //Playing a sound for chosen colour.
 function playSound(whichColour) {
   let audio = new Audio(`sounds/${whichColour}.mp3`);
   audio.addEventListener("canplaythrough", (e) => {
     audio.play();
-    // console.log(randomChosenColour);
-    // console.log(`sounds/${whichColour}.mp3`);
-    console.log(gamePattern);
   });
 }
 
 //Detecting when a button is clicked, but with event delegation (DIV <- DIV <- BUTTON).
 function clickHandler() {
   $(".container").on("click", (e) => {
-    // console.log(`I'm got clicked. But I feel that just like bubbles! :)`);
-    if (e.target.tagName === "BUTTON") {
-      // console.log(e.target.id);
+    if (e.target.tagName === "BUTTON" && gameStarted === true) {
       let userChosenColour = e.target.id;
       userClickedPattern.push(userChosenColour);
-      console.log(userClickedPattern);
       $(`#${userChosenColour}`).on("click", playSound(userChosenColour));
       animatePress(userChosenColour);
+      // setInterval(() => updateUI("I'm thinking..."), 2000);
+      setTimeout(() => {
+        updateUI(`Level ${level}`);
+        nextSequence();
+      }, 3000);
+
+      console.log(gamePattern);
+      console.log(userClickedPattern);
     }
   });
 }
@@ -62,7 +83,22 @@ function clickHandler() {
 function animatePress(currentColour) {
   console.log("hi");
   $(`#${currentColour}`).addClass("pressed");
-  setTimeout(() => $(`#${currentColour}`).toggleClass("pressed"), 100);
+  setTimeout(() => $(`#${currentColour}`).removeClass("pressed"), 100);
 }
 
 clickHandler();
+
+/* const myFirstPromise = new Promise((resolve, reject) => {
+  // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
+  // In this example, we use setTimeout(...) to simulate async code.
+  // In reality, you will probably be using something like XHR or an HTML API.
+  setTimeout(() => {
+    resolve("Success!"); // Yay! Everything went well!
+  }, 10000);
+});
+
+myFirstPromise.then((successMessage) => {
+  // successMessage is whatever we passed in the resolve(...) function above.
+  // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+  console.log(`Yay! ${successMessage}`);
+}); */
