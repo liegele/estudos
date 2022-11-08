6e94d367490815b1fc2c3e83752c37864a812235
liegele <liegele@gmail.com>
2022-11-08 17:00:59 -0300

    Versão com bugs da função showGamingPattern corrigidos

--- the-complete-2022-web-development-bootcamp/section-15-boss-level-challenge-the-simon-game/script.js
+++ the-complete-2022-web-development-bootcamp/section-15-boss-level-challenge-the-simon-game/script.js
@@ -33,7 +33,10 @@ $("body").on("keydown", (e) => {
     updateUI(`Level ${level}`, "Good luck!");
     gamePattern = [];
     nextSequence();
-    // showGamingPattern();
+    showGamingPattern();
+    setTimeout(() => {
+      updateUI(`Level ${level}`);
+    });
   }
 });
 
@@ -55,15 +58,17 @@ function nextSequence() {
   randomNumber = Math.floor(Math.random() * 4);
   randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  updateUI(`Level ${level}`);
-  // level++;
-  initialLevelClickHandler = 1;
-  showGamingPattern();
+  // updateUI(`Level ${level}`);
+  // initialLevelClickHandler = 1;
+  // showGamingPattern();
   logger("nextSequence");
 }
 
 //Running game pattern until actual level.
 function showGamingPattern() {
+  console.log(
+    `inside showGamingPattern: Level: ${level} / Step: ${step} / (${initialLevelClickHandler} === ${level})`
+  );
   if (initialLevelClickHandler === level) {
     gameStarted = false;
     gamePattern.forEach((element, index) => {
@@ -101,16 +106,15 @@ function clickHandler() {
       animatePress(userChosenColour);
       console.log(initialLevelClickHandler, level);
 
-      console.log(compareSequence(step));
-      if (compareSequence(step)) {
+      console.log(`compareSequence: ${compareSequence()}`);
+      if (compareSequence()) {
         if (initialLevelClickHandler === level) {
           logger("clickHandler");
           console.log("+++++++++++++++++++++++++++++++");
-          updateUI(`Level ${level}`, "Creating new sequence...");
+          updateUI(`Level ${level}`, "Nice job! Creating new sequence...");
           setTimeout(() => {
             nextSequence();
-            clearTimeout();
-            // showGamingPattern();
+            showGamingPattern();
             updateUI(`Level ${level}`);
             level++;
             console.log(gamePattern);
@@ -119,37 +123,44 @@ function clickHandler() {
             initialLevelClickHandler = 1;
             step = 0;
             updateUI(`Level ${level}`);
-          }, 4000);
+          }, 3000);
+        } else {
+          initialLevelClickHandler++;
+          step++;
+          updateUI(`Level ${level}`);
         }
       } else {
         gameOver();
       }
-
-      initialLevelClickHandler++;
-      // step++;
     }
   });
 }
 
 //Compares between game sequence against user sequence.
-function compareSequence(actualLevel) {
+
+function compareSequence() {
   logger("compareSequence");
-  if (gamePattern[actualLevel] === userClickedPattern[actualLevel]) {
+  let compareResult = true;
+  for (let i = 0; i < userClickedPattern.length; i++) {
+    console.log(
+      `#${i}:gamePattern: ${gamePattern[i]}  / userClickedPattern: ${userClickedPattern[i]}`
+    );
+    if (gamePattern[i] !== userClickedPattern[i]) {
+      compareResult = false;
+      break;
+    } else {
+      compareResult = true;
+    }
+  }
+  return compareResult;
+}
+
+/* function compareSequence(actualLevel) {
+  logger("compareSequence");
+  if (gamePattern[level - 1] === userClickedPattern[level - 1]) {
     return true;
   } else {
     return false;
-  }
-}
-
-/* function compareSequence() {
-  logger("compareSequence");
-  for (let i = 0; i < gamePattern.length; i++) {
-    logger("compareSequence inside for loop");
-    if (gamePattern[i] !== userClickedPattern[i]) {
-      return false;
-    } else {
-      return true;
-    }
   }
 } */
 
@@ -167,52 +178,3 @@ function animatePress(currentColour) {
 }
 
 clickHandler();
-
-/* const myFirstPromise = new Promise((resolve, reject) => {
-  // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
-  // In this example, we use setTimeout(...) to simulate async code.
-  // In reality, you will probably be using something like XHR or an HTML API.
-  setTimeout(() => {
-    resolve("Success!"); // Yay! Everything went well!
-  }, 10000);
-});
-
-myFirstPromise.then((successMessage) => {
-  // successMessage is whatever we passed in the resolve(...) function above.
-  // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
-  console.log(`Yay! ${successMessage}`);
-}); */
-
-/*  gamePattern.forEach((element) => {
-    setTimeout(() => {
-      $(`#${element}`).delay().fadeOut().fadeIn();
-      playSound(element);
-    }, 1000);
-  }); */
-
-/* gamePattern.forEach(() => {
-    new Promise((resolve) => {
-      resolve(setTimeout(() => {}, 1000));
-    }).then((element) => {
-      setTimeout(() => {
-        $(`#${element}`).delay(100).fadeOut().fadeIn();
-        playSound(element);
-        console.log(element);
-      }, 0);
-    });
-  });
- */
-/* for (const value of gamePattern) {
-    $(`#${value}`).delay().fadeOut().fadeIn();
-    playSound(value);
-    sleep(2000);
-  } */
-
-/* gamePattern.forEach(() => {
-    new Promise((resolve) => {
-      resolve(setTimeout(() => {}, 1000));
-    }).then((element) => {
-      $(`#${element}`).delay(100).fadeOut().fadeIn();
-      playSound(element);
-    });
-  }); */
