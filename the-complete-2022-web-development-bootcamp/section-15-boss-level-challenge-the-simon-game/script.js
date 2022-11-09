5b8b8d889bc2aedea54a0413d6f5550c51fea4d4
liegele <liegele@gmail.com>
2022-11-08 21:44:38 -0300

    Versão com design atualizado, botão Start e bordas arredondadas nos botões coloridos.

--- the-complete-2022-web-development-bootcamp/section-15-boss-level-challenge-the-simon-game/script.js
+++ the-complete-2022-web-development-bootcamp/section-15-boss-level-challenge-the-simon-game/script.js
@@ -2,43 +2,33 @@ let gamePattern = [];
 let userClickedPattern = [];
 let buttonColours = ["red", "blue", "green", "yellow"];
 let randomNumber, randomChosenColour;
-let level = 1;
-let initialLevelClickHandler = 1;
 let gameStarted = false;
-let step = 0;
-let index = 1;
 
-function logger(msg) {
-  /* console.log(`#${index} ${msg}`);
-  console.log(`level: ${level}`);
-  console.log(`InitialLevelClickHandler: ${initialLevelClickHandler}`);
-  console.log(`gamePattern: ${gamePattern}`);
-  console.log(`userClickedPattern: ${userClickedPattern}`);
-  console.log(`------------------------------------`);
-  index++; */
-  /* console.log(``);
-  console.log(``);
-  console.log(``);
-  console.log(``);
-  console.log(``);
-  console.log(``);
-  console.log(``); */
-}
+//Starting game when Start button is clicked.
 
-//Starting game when 'A' key is pressed.
-$("body").on("keydown", (e) => {
-  if (e.key === "a" && gameStarted === false) {
-    logger("pressing A");
-    gameStarted = true;
+$("#level-title").on("click", (e) => {
+  console.log($("button").text());
+  if (gameStarted === false) {
+    startingGame();
     updateUI(`Level ${level}`, "Good luck!");
-    gamePattern = [];
-    nextSequence();
-    showGamingPattern();
     setTimeout(() => {
+      nextSequence();
+      showGamingPattern();
       updateUI(`Level ${level}`);
-    });
+      gameStarted = true;
+    }, 2000);
   }
 });
+
+function startingGame() {
+  gameStarted = true;
+  gamePattern = [];
+  userClickedPattern = [];
+  level = 1;
+  initialLevelClickHandler = 1;
+  step = 0;
+  index = 1;
+}
 
 //Update the user interface.
 function updateUI(message, supportText) {
@@ -58,17 +48,10 @@ function nextSequence() {
   randomNumber = Math.floor(Math.random() * 4);
   randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-  // updateUI(`Level ${level}`);
-  // initialLevelClickHandler = 1;
-  // showGamingPattern();
-  logger("nextSequence");
 }
 
 //Running game pattern until actual level.
 function showGamingPattern() {
-  console.log(
-    `inside showGamingPattern: Level: ${level} / Step: ${step} / (${initialLevelClickHandler} === ${level})`
-  );
   if (initialLevelClickHandler === level) {
     gameStarted = false;
     gamePattern.forEach((element, index) => {
@@ -77,7 +60,6 @@ function showGamingPattern() {
         playAndEffectButton(element);
       }, index * 1000);
     });
-    logger("showGamingPattern");
   }
   gameStarted = true;
 }
@@ -104,21 +86,17 @@ function clickHandler() {
       userClickedPattern.push(userChosenColour);
       $(`#${userChosenColour}`).on("click", playSound(userChosenColour));
       animatePress(userChosenColour);
-      console.log(initialLevelClickHandler, level);
 
-      console.log(`compareSequence: ${compareSequence()}`);
       if (compareSequence()) {
         if (initialLevelClickHandler === level) {
           logger("clickHandler");
           console.log("+++++++++++++++++++++++++++++++");
-          updateUI(`Level ${level}`, "Nice job! Creating new sequence...");
+          updateUI(`Level ${level}`, "Nice job! Next level is coming...");
           setTimeout(() => {
             nextSequence();
             showGamingPattern();
             updateUI(`Level ${level}`);
             level++;
-            console.log(gamePattern);
-            console.log(userClickedPattern);
             userClickedPattern = [];
             initialLevelClickHandler = 1;
             step = 0;
@@ -131,6 +109,8 @@ function clickHandler() {
         }
       } else {
         gameOver();
+        startingGame();
+        gameStarted = false;
       }
     }
   });
@@ -139,12 +119,11 @@ function clickHandler() {
 //Compares between game sequence against user sequence.
 
 function compareSequence() {
-  logger("compareSequence");
   let compareResult = true;
   for (let i = 0; i < userClickedPattern.length; i++) {
-    console.log(
+    /* console.log(
       `#${i}:gamePattern: ${gamePattern[i]}  / userClickedPattern: ${userClickedPattern[i]}`
-    );
+    ); */
     if (gamePattern[i] !== userClickedPattern[i]) {
       compareResult = false;
       break;
@@ -155,18 +134,9 @@ function compareSequence() {
   return compareResult;
 }
 
-/* function compareSequence(actualLevel) {
-  logger("compareSequence");
-  if (gamePattern[level - 1] === userClickedPattern[level - 1]) {
-    return true;
-  } else {
-    return false;
-  }
-} */
-
 //Game over handler
 function gameOver() {
-  updateUI("GAME OVER!", "Best luck next time!");
+  updateUI("Restart", "GAME OVER!");
   playSound("wrong");
   gameStarted = false;
 }
