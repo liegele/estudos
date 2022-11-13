01e566e4c2b54baa025e54960a3884197aaca738
liegele <liegele@gmail.com>
2022-11-12 23:57:07 -0300

    Atualizando arquivos.

--- the-complete-2022-web-development-bootcamp/section-19-express-js-with-node-js/calculator/calculator.js
+++ the-complete-2022-web-development-bootcamp/section-19-express-js-with-node-js/calculator/calculator.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
   //   console.log(__dirname);
+});
+
+app.post("/", (req, res) => {
+  //   console.log(req.body);
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let result = num1 + num2;
+  res.send(`Result of operation is: ${result}`);
 });
 
 app.listen(port, () => {
