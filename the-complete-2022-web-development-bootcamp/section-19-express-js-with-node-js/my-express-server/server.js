28219118682f4f7ea29ab0a0c601c7ac6d49f888
liegele <liegele@gmail.com>
2022-11-12 22:36:38 -0300

    Atualizando arquivos.

--- the-complete-2022-web-development-bootcamp/section-19-express-js-with-node-js/my-express-server/server.js
+++ the-complete-2022-web-development-bootcamp/section-19-express-js-with-node-js/my-express-server/server.js
@@ -6,6 +6,17 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World from ExpressJS server, Liégele!</h1>");
 });
 
+app.get("/contact", (erq, res) => {
+  res.send("<h1>Contact me at liegele@gmail.com</h1>");
+  // console.log("Contact me at liegele@gmail.com");
+});
+
+app.get("/about", (req, res) => {
+  res.send(
+    "<h1>Hi there! I'm Liégele. I'm studing to get a IT job. I'm thinking about Backend development.</h1>"
+  );
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
