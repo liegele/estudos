d36c8104de9682e0111197c3383fd9816f523ba4
liegele <liegele@gmail.com>
2022-11-19 20:39:11 -0300

    Obtendo dados do formulário via body-parser.

--- the-complete-2022-web-development-bootcamp/section-20-apis-application-programming-interfaces/newsletter-signup/app.js
+++ the-complete-2022-web-development-bootcamp/section-20-apis-application-programming-interfaces/newsletter-signup/app.js
@@ -12,6 +12,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/signup.html');
 });
 
+app.post('/', (req, res) => {
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let email = req.body.email;
+  console.log(`${firstName} :: ${lastName} :: ${email}`);
+});
+
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
 });
