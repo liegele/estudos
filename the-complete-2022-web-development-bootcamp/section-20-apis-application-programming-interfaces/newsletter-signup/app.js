ed37b666469b2f4dc0c7cf690feb24686c9d7ffe
liegele <liegele@gmail.com>
2022-11-22 10:32:59 -0300

    Atualizando arquivos.

--- the-complete-2022-web-development-bootcamp/section-20-apis-application-programming-interfaces/newsletter-signup/app.js
+++ the-complete-2022-web-development-bootcamp/section-20-apis-application-programming-interfaces/newsletter-signup/app.js
@@ -31,22 +31,51 @@ app.post('/', (req, res) => {
     ],
   };
 
-  // const jsonData = JSON.stringify(data);
+  console.log(data.members[0]);
 
-  const listId = '';
-  async function run() {
-    const response = await mailchimp.lists.addListMember(
-      listId,
-      data.members[0]
-    );
-    console.log(
-      `Successfully added contact as an audience member. The contact\'s id is ${response.id}`
-    );
-  }
+  const jsonData = JSON.stringify(data);
 
-  console.log(`${firstName} :: ${lastName} :: ${email}`);
+  const url = `https://us14.api.mailchimp.com/3.0/lists/c439257566./members`;
+  // const url = `https://us14.api.mailchimp.com/schema/3.0/lists/c439257566./members`;
 
-  run();
+  const options = {
+    method: 'POST',
+    auth: 'liegele: b8ec24a3e4040e3feb38c51695494f28-us14',
+  };
+
+  const request = https.request(url, options, (response) => {
+    response.on('data', (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+
+  // mailchimp.setConfig({
+  //   apiKey: 'b8ec24a3e4040e3feb38c51695494f28',
+  //   server: 'us14',
+  // });
+
+  // const listId = 'c439257566.';
+  // async function run() {
+  //   // const response = await mailchimp.lists.batchListMembers(listId, {
+  //   const response = await mailchimp.lists.addListMember(listId, {
+  //     email_address: data.members[0].email_address,
+  //     status: 'subscribed',
+  //     merge_fields: {
+  //       FNAME: data.members[0].FNAME,
+  //       LNAME: data.members[0].LNAME,
+  //     },
+  //   });
+  //   console.log(
+  //     `Successfully added contact as an audience member. The contact\'s id is ${response.id}`
+  //   );
+  // }
+
+  // console.log(`${firstName} :: ${lastName} :: ${email}`);
+
+  // run();
 
   /* mailchimp.setConfig({
     apiKey: '',
