d84ceaace40f4185ec94bf67eaadfac463367ad6
liegele <liegele@gmail.com>
2023-01-09 21:48:25 -0300

    Atualizando arquivos

--- javascript/the_complete_javascript_course_2022_from_zero_to_expert/10-Functions/starter/script.js
+++ javascript/the_complete_javascript_course_2022_from_zero_to_expert/10-Functions/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const bookings = [];
+
+//Default parameters...
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 200 * numPassengers
+) {
+  /* const booking = {
+    flightNum: flightNum,
+    numPassengers: numPassengers,
+    price: price,
+  }; */
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
