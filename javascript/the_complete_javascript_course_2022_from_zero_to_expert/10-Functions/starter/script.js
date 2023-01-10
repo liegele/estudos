4c85052b1548992306e11754f9c8c375929b79a8
liegele <liegele@gmail.com>
2023-01-09 22:11:43 -0300

    Atualizando arquivos

--- javascript/the_complete_javascript_course_2022_from_zero_to_expert/10-Functions/starter/script.js
+++ javascript/the_complete_javascript_course_2022_from_zero_to_expert/10-Functions/starter/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
+//Default parameters...
+
 const bookings = [];
 
-//Default parameters...
 const createBooking = function (
   flightNum,
   numPassengers = 1,
@@ -27,3 +28,33 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+
+//How passing arguments works: Valeu versus Reference...
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
