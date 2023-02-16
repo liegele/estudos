e541f22b52a4f184e1656195d8285ae4cdef0f16
liegele <liegele@gmail.com>
2023-02-16 12:00:51 -0300

    Funções Adicionar, Ler e Deletar implementadas. Falta Atualizar dados.

--- freecodecamp/firebase-firestore-freecodecamp-example/app.js
+++ freecodecamp/firebase-firestore-freecodecamp-example/app.js
@@ -12,14 +12,81 @@ firebase.initializeApp({
 const db = firebase.firestore();
 
 //Getting HTML elements
-const username = document.getElementById('username');
-const about = document.getElementById('about');
+let username = document.getElementById('username');
+let about = document.getElementById('about');
 const btn = document.getElementById('btn');
+const lists = document.getElementById('lists');
 
+//Adding data to Firestore database
 btn.addEventListener('click', (e) => {
   e.preventDefault();
   console.log(db.collection('userInfo'));
   db.collection('userInfo').add({ username: username.value, bio: about.value });
   username.value = about.value = '';
-  alert('Cadastro realizado.');
+  // alert('Cadastro realizado.');
+  getUsers();
 });
+
+//Reading users from userInfo collection
+const getUsers = function () {
+  lists.textContent = '';
+  db.collection('userInfo')
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        let li = document.createElement('li');
+        let userId = document.createElement('h6');
+        let usernameH4 = document.createElement('h4');
+        let bio = document.createElement('p');
+        let selectUserButton = document.createElement('button');
+        let deleteUserButton = document.createElement('button');
+
+        userId.textContent = doc.id;
+        usernameH4.textContent = doc.data().username;
+        bio.textContent = doc.data().bio;
+        //Creating Select button
+        selectUserButton.setAttribute('type', 'button');
+        selectUserButton.innerText = 'Select';
+        selectUserButton.addEventListener('click', () => {
+          console.log(doc.data().username, doc.data().bio);
+          username.value = doc.data().username;
+          about.innerText = doc.data().bio;
+        });
+        //Creating Delete button
+        deleteUserButton.setAttribute('type', 'button');
+        deleteUserButton.innerText = 'Delete';
+        deleteUserButton.addEventListener('click', () => {
+          deleteUsers(doc.id);
+        });
+
+        li.appendChild(userId);
+        li.appendChild(usernameH4);
+        li.appendChild(bio);
+        li.appendChild(selectUserButton);
+        li.appendChild(deleteUserButton);
+
+        lists.appendChild(li);
+      });
+    })
+    .catch((error) => {
+      console.log('Error getting documents: ', error);
+    });
+};
+
+getUsers();
+
+//Update user data from userInfo collection
+const updateUsers = function (docId) {
+  //Add code here...
+  db.collection('userInfo')
+    .doc(docId)
+    .update({ username: username.value, bio: about.value });
+  getUsers();
+};
+
+//Delete user data from userInfo collection
+const deleteUsers = function (docId) {
+  //Add code here...
+  db.collection('userInfo').doc(docId).delete();
+  getUsers();
+};
