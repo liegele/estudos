88f2f5d1590c75ba2036000df8250200938d1b81
liegele <liegele@gmail.com>
2023-03-29 15:13:42 -0300

    Connectando ao MongoDB, mongoose e colors

---
+++ devcamper-api/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  });
+
+  console.log(`MongDB connected: ${conn.connection.host}`.cyan.bold);
+};
+
+module.exports = connectDB;
