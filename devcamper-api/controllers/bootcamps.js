50cf8e7224049aac0586c493d91c8169940f49df
liegele <liegele@gmail.com>
2023-03-29 22:43:31 -0300

    Criando novos Bootcamps via mongoose no MongoDB

--- devcamper-api/controllers/bootcamps.js
+++ devcamper-api/controllers/bootcamps.js
@@ -1,3 +1,6 @@
+//Loading Bootcamp model
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
@@ -18,8 +21,18 @@ exports.getBootcamp = (req, res, next) => {
 // @desc        Create new bootcamp
 // @route       POST /api/v1/bootcamps
 // @access      Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+    res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc        Update bootcamp
