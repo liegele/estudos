0e5a58686eb3431798598eb5e2191d72f01d2a9d
liegele <liegele@gmail.com>
2023-04-07 23:40:38 -0300

    Implementando form

--- graphbook/src/client/App.js
+++ graphbook/src/client/App.js
@@ -1,26 +1,6 @@
 import '../../assets/css/style.css';
 import React, { useState } from 'react';
 
-const App = () => {
-  const [posts, setPosts] = useState(initialPosts);
-
-  return (
-    <div className="container">
-      <div className="feed">
-        {initialPosts.map((post, i) => (
-          <div id={post.id} className="post">
-            <div className="header">
-              <img src={post.user.avatar} />
-              <h2>{post.user.username}</h2>
-            </div>
-            <p className="content">{post.text}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
 const initialPosts = [
   {
     id: 2,
@@ -39,5 +19,50 @@ const initialPosts = [
     },
   },
 ];
+
+const App = () => {
+  const [posts, setPosts] = useState(initialPosts);
+  const [postContent, setPostContent] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPost = {
+      id: posts.length + 1,
+      text: postContent,
+      user: {
+        avatar: '/uploads/avatar1.png',
+        username: 'Fake User',
+      },
+    };
+    setPosts([newPost, ...posts]);
+    setPostContent('');
+  };
+
+  return (
+    <div className="container">
+      <div className="postForm">
+        <form onSubmit={handleSubmit}>
+          <textarea
+            value={postContent}
+            onChange={(e) => setPostContent(e.target.value)}
+            placeholder="Write your custom post!"
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      <div className="feed">
+        {initialPosts.map((post, i) => (
+          <div key={post.id} className="post">
+            <div className="header">
+              <img src={post.user.avatar} />
+              <h2>{post.user.username}</h2>
+            </div>
+            <p className="content">{post.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
