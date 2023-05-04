f65ba26d903537fd1dd9f1e4e9c52da330b4037d
liegele <liegele@gmail.com>
2023-05-04 17:28:51 -0300

    Adicionando pasta e arquivos iniciais

---
+++ three-js-journey/script.js
@@ -0,0 +1,29 @@
+//Scene
+const scene = new THREE.Scene();
+
+//Getting canvas element in HTML
+const canvas = document.querySelector('canvas.webgl');
+
+//Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+//Adding object to the scene
+scene.add(mesh);
+
+//Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+scene.add(camera);
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
