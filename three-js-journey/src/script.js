20b610ef765c2cc8860ec368351b1e3c46b45f28
liegele <liegele@gmail.com>
2023-05-08 19:05:31 -0300

    Versão completa do capítulo Fullscreen and resizing

--- three-js-journey/src/script.js
+++ three-js-journey/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import gsap from 'gsap';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 //Scene
 const scene = new THREE.Scene();
@@ -48,17 +49,54 @@ group.add(cube3); */
 
 //Sizes
 const sizes = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
+
+//Handle resizing screen
+window.addEventListener('resize', () => {
+  //Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  //Upate camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  //Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+//Handle fullscreen
+window.addEventListener('dblclick', () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+});
 
 //Camera
 const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
-  1,
+  0.1,
   100
 );
+
+// console.log(window.devicePixelRatio);
 
 /* const aspectRatio = sizes.width / sizes.height;
 const camera = new THREE.OrthographicCamera(
@@ -75,7 +113,7 @@ scene.add(camera);
 
 //Cursor
 
-const cursor = {
+/* const cursor = {
   x: 0,
   y: 0,
 };
@@ -84,7 +122,11 @@ window.addEventListener('mousemove', (event) => {
   cursor.x = event.clientX / sizes.width - 0.5;
   cursor.y = -(event.clientY / sizes.height - 0.5);
   console.log(cursor.x, cursor.y);
-});
+}); */
+
+//Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 //Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -107,13 +149,19 @@ const tick = () => {
   mesh.position.y = Math.sin(elapseTime); */
   // camera.lookAt(mesh.position);
 
-  //Update camera
+  /* //Update camera
   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
-  camera.position.y = cursor.y * 3;
+  camera.position.y = cursor.y * 3; */
   // camera.position.x = cursor.x * 5;
   // camera.position.y = cursor.y * 5;
-  camera.lookAt(mesh.position);
+  // camera.lookAt(mesh.position);
+
+  /* //Redefining Target property
+  controls.target.y = -2;
+  controls.update(); */
+
+  controls.update();
 
   //Render
   renderer.render(scene, camera);
